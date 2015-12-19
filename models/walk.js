7e68eb594b9ac398d268571677e3769324ba812c
Dustin <dbruce1990@gmail.com>
2015-12-19 14:39:48 -0800

    Talking to database, retrieving WalkModels and creating them, set up to write any CRUD operations I need.

--- models/walk.js
+++ models/walk.js
@@ -2,7 +2,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var Walk = new Schema({
-  description: String
+  id: Schema.ObjectId,
+  description: String,
+  createdAt: Number,
+  elapsedTime: Number,
+  distance: Number,
+  waypoints: Array
 });
 
 module.exports = mongoose.model('Walk', Walk);
--- routes/walks.js
+++ routes/walks.js
@@ -4,15 +4,24 @@ var mongoose = require('mongoose');
 var Walk = require('../models/walk.js');
 
 router.get('/', function(req, res){
-  res.send("Hello from /walk");
+  Walk.find().sort({createdAt: -1}).exec(function(err, results){
+    if(err){
+      throw err;
+      console.log(err);
+    }
+    res.json(results);
+  });
 });
 
-router.post('/create', function (req, res, next) {
-  console.log(req.body);
-
+router.post('/', function (req, res, next) {
   var walk = new Walk();
   walk.description = req.body.description;
+  walk.createdAt = req.body.createdAt;
+  walk.elapsedTime = req.body.elapsedTime;
+  walk.distance = req.body.distance;
+  walk.waypoints = req.body.waypoints;
 
+  console.log(walk);
   walk.save(function(err, walk){
     if(err){
       console.log(err);
@@ -22,5 +31,7 @@ router.post('/create', function (req, res, next) {
     }
   });
 })
+
+
 
 module.exports = router;
