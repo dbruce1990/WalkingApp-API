b219fd4021866c38f65435f34cada293d95f10f0
janedoe <dbruce1990@gmail.com>
2016-01-02 23:22:22 -0800

    wrote tests for posting missing credentials to /signup. ended with refactoring handlers/error.js. Could still use some more refactoring, but going to wait and see how it develops before changing it anymore at the moment.

--- handlers/error.js
+++ handlers/error.js
@@ -1,11 +1,44 @@
 module.exports = function(res, err){
-  switch(err.code){
-    case 11000:
-      return res.status(500).send({
-        success: false,
-        message: 'Duplicate found.'
-      });
-    default:
-      return res.status(500).send('Something appears to have gone wrong.');
+  var response = {
+    success: false,
+    message: ""
+  };
+
+  var defaultResponse = function(){
+    return 'Something appears to have gone wrong.';
+  };
+  var errorCodes = function(){
+    switch(err.code){
+      case 11000:
+        return 'Duplicate found.';
+      default:
+        return 'duh something went wrong?!?!';
+    }
+  };
+
+  var userValidationErrors = function(){
+    if(typeof err.errors.username !== 'undefined'){
+      if(typeof err.errors.username.message !== 'undefined')
+        return err.errors.username.message;
+    }
+    else if(typeof err.errors.password !== 'undefined'){
+      if(typeof err.errors.password.message !== 'undefined')
+        return err.errors.password.message;
+    }
+    else
+      return "User validation failed";
+  };
+
+  if(typeof err.code !== 'undefined'){
+    response.message = errorCodes();
   }
+  else{
+    if(err.message == "User validation failed"){
+      response.message = userValidationErrors();
+    }
+    else{
+      response.message = defaultResponse();
+    }
+  }
+  return res.status(500).send(response);
 };
