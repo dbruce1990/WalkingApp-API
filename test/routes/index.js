10d96d986d9cf7a1125481a26db4f679ae2e2461
janedoe <dbruce1990@gmail.com>
2016-01-02 17:21:41 -0800

    added test for login page and signup page, corrected package.json

--- test/routes/index.js
+++ test/routes/index.js
@@ -1,11 +1,42 @@
 var app = require('../../app');
-var agent = require('supertest').agent(app);
+var should = require('should');
+var req = require('supertest')(app);
 
-describe('Index Routes', function(){
+describe.only('Index Routes', function(){
   it('should return 200', function(done){
-      agent.get('/').expect(200, done);
+      req.get('/')
+        .expect(200, done);
   });
-    it('should fail horribly', function(done){
-    agent.get('/fail').expect(401, done);
+
+  it('should fail horribly', function(done){
+    req.get('/fail')
+      .expect(401, done);
+  });
+
+  it('should return login page', function(done){
+    req.get('/login')
+      .expect(200)
+      .end(function(err, res){
+        if(err) return done(err);
+        res.text.should.match(/<form name="login" method="post">/);
+        res.text.should.match(/name="login"/);
+        res.text.should.match(/name="username"/);
+        res.text.should.match(/name="password"/);
+        res.text.should.match(/name="submit"/);
+        done();
+      });
+  });
+
+  it('should return signup page', function(done){
+    req.get('/signup')
+      .expect(200)
+      .end(function(err, res){
+        if(err) return done(err);
+        res.text.should.match(/<form name="signup" method="post">/);
+        res.text.should.match(/name="username"/);
+        res.text.should.match(/name="password"/);
+        res.text.should.match(/name="submit"/);
+        done();
+      });
   });
 });
