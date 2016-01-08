40f35aae66ffaaae77a416b38d4296561854d82a
janedoe <dbruce1990@gmail.com>
2016-01-08 02:53:08 -0800

    removing garbage files to allow bfg to properly clean old commits since it doesn't clean most recent one

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+  production: 'mongodb://admin:m0ngol4bs@ds035485.mongolab.com:35485/heroku_8xb6vq0w',
+  development: 'mongodb://localhost:27017/WalkingApp-Dev',
+  test: 'mongodb://localhost:27017/WalkingApp-Test'
+};
